dfd7e7609e25f810abf19733b595c830810797c7
Gyver <gb033519@ohio.edu>
2019-09-06 15:11:48 -0400

    message

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gyver Blessing
+ *        Date: 9/6/2019
+ * Description: Shows hello git!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+   cout << "Hello Git!!" << endl;
+    return 0;
+}// main
